4c8b0ba23d3059204837972eaf95f7e40ae76fc4
hiroto-hara <xre0605@gmail.com>
2024-07-24 22:15:58 +0900

    ページの雛形作成

---
+++ pages/WeatherInfo/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { City } from "@/interface";
+
+const route = useRoute();
+const cityList = useState<Map<number, City>>("cityList");
+const selectedCity = computed(
+  (): City => {
+    const idNo = Number(route.params.id);
+    return cityList.value.get(idNo) as City;
+  }
+);
+const weatherDescription = ref("");
+</script>
+
+<template>
+  <section>
+    <h2>{{ selectedCity.name }}の天気</h2>
+    <p>{{ weatherDescription }}</p>
+  </section>
+  <p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
+</template>
